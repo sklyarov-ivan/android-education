14be9437a9d2d2d3e9d3a80e7266240aa78d4b8d
Ivan Sklyarov <sklyarov.161@gmail.com>
2014-10-25 22:34:56 +0400

    add loading activity

--- app/src/main/java/local/localhost/education/education/LoadActivity.java
+++ app/src/main/java/local/localhost/education/education/LoadActivity.java
@@ -1,31 +1,41 @@
 package local.localhost.education.education;
 
 import android.app.Activity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.content.Intent;
 
 public class LoadActivity extends Activity {
-
+    MediaPlayer loadSong;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_load);
+        loadSong = MediaPlayer.create(LoadActivity.this, R.raw.load_song);
+        loadSong.start();
         Thread timer = new Thread(){
             @Override
             public void run() {
                 super.run();
                 try {
-
+                    sleep(5000);
                 } catch (InterruptedException e) {
-
+                    e.fillInStackTrace();
                 } finally {
-
+                    Intent counterActivity = new Intent("local.localhost.education.education.COUNTERACTIVITY");
+                    startActivity(counterActivity);
                 }
             }
         };
         timer.start();
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+        loadSong.release();
+    }
 }
